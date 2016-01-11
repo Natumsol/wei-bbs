7d84b4ac8585aa4c625cbc17ad99ffd89180514f
natumsol <natumsol@gmail.com>
2016-01-11 20:04:00 +0800

    add test case

---
+++ test/db.js
@@ -0,0 +1,14 @@
+/**
+ *@description:
+ *@author: LiuJ
+ *@date: 2016/1/11
+ */
+require("../config/mongoose.js")();
+var mongoose = require("mongoose");
+var User = mongoose.model("User");
+User.findOne({_id:"568e6e9514c8f84806d0c158"}, function(err, user){
+    if(err) throw  err;
+    if(!user) console.log("查询为空！");
+    else console.log(user);
+    process.exit();
+});
