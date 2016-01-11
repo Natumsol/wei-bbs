159ecc26a07a641ec1d136f23bd471c079b30090
natumsol <natumsol@gmail.com>
2016-01-11 20:01:51 +0800

    finished column module

--- app/models/column.server.model.js
+++ app/models/column.server.model.js
@@ -4,7 +4,23 @@
  */
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
+var xss = require("xss");
 var ColumnSchema = new Schema({
-
+    name: String,
+    date: {
+        type: Date,
+        default:Date.now
+    },
+    isHot: {
+        type: Boolean,
+        default: false
+    }
 });
+
+ColumnSchema.pre("save", function(next){
+    this.name = xss( this.name.trim());
+    next();
+});
+
+
 mongoose.model('Column', ColumnSchema);
