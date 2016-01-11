c80406ee894cbc1e6b1d1f5fdf8edd76f5606d5c
natumsol <natumsol@gmail.com>
2016-01-11 21:55:57 +0800

    finish article router

--- app/routes/article.server.route.js
+++ app/routes/article.server.route.js
@@ -3,3 +3,13 @@
  *@author: LiuJ
  *@date: 2016/1/11
  */
+var article = require("../controllers/article.server.controller.js");
+module.exports = function (app) {
+    var nameSpace = "/article";
+    app.post(nameSpace + "/add", article.add);
+    app.post(nameSpace + "/modify", article.modify);
+    app.post(nameSpace + "/delete", article.remove);
+    app.post(nameSpace + "/list", article.list);
+    app.post(nameSpace + "/addComment", article.addComment);
+    app.post(nameSpace + "/deleteComment", article.deleteComment);
+};
