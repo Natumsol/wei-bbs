99e56c6d89428c329d22f2e12ecaea123171cbbf
natumsol <natumsol@gmail.com>
2016-03-13 16:47:10 +0800

    add cluster

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
-process.env.NODE_ENV  = process.env.NODE_ENV || "development";
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+var cluster = require('cluster');
+var http = require('http');
+var numCPUs = require('os').cpus().length;
 var express = require("./config/express");
 var db = require("./config/mongoose.js")();
 var app = express();
-app.listen(80);
+// 启动应用并开启监听端口
+if (cluster.isMaster) {
+    console.log('宿主启动...');
+
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+    cluster.on('listening', function (worker, address) {
+        console.log('核心' + i + ' pid:' + worker.process.pid);
+    });
+    cluster.on('exit', function (worker, code, signal) {
+        console.log('核心' + i + ' pid:' + worker.process.pid + ' 重启');
+        setTimeout(function () {
+            cluster.fork();
+        }, 2000);
+    });
+} else {
+    app.listen(80);
+}
+//app.listen(80);
 module.exports = app;
 console.log("Server running at http://localhost.\n" + "using version: " + process.env.NODE_ENV);
