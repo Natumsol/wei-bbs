d88e5e2fa88860672a56829048a51132ab886269
natumsol <natumsol@gmail.com>
2016-03-29 09:11:40 +0800

    fix bugs

--- app/routes/about.server.route.js
+++ app/routes/about.server.route.js
@@ -7,7 +7,7 @@ var about = require("../controllers/about.server.controller.js");
 var weixinAuth = require("../controllers/user.server.controller.js");
 module.exports = function (app) {
     var nameSpace = "/about";
-    app.post(nameSpace + "/modify", weixinAuth.checkAuth, about.modify);
+    app.post(nameSpace + "/modify", weixinAuth.checkAdminLogin, about.modify);
     app.get(nameSpace, function(req, res){
         about.getAbout(function(err, about){
             res.render("about",{
